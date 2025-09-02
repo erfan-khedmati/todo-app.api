0b378db5ab03083005c5e63a84b4ca23fb2f44d9
Erfan Khedmati <100248103+EranKhedmati@users.noreply.github.com>
2025-09-02 15:47:51 +0330

    create base server and connection to data base

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from 'cors';
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+
+const ConnectionString = process.env.DB_CONNECTION_STRING;
+
+if (ConnectionString) {
+    mongoose.connect(ConnectionString, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then(() => {
+        console.log("DB connected");
+        app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
+    }).catch(err => console.log(err))
+} else {
+    console.error("bad getway");
+}
